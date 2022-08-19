b1c2246b54bd07c1fd5b29bc52463396f80b795c
Miconbeko <stepanov.m2004@gmail.com>
2022-08-19 15:51:45 +0300

    First commit

---
+++ VSC/head.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void printHW()
+{
+    std::cout << "Hello world!(2)" << std::endl;
+}
---
+++ VSC/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "head.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl << endl;
+    printHW();
+
+    return 0;
+}
